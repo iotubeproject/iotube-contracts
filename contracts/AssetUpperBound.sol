2de0aedb74c04cd08ecb69a1378e1efd0ae81aaf
zhi <thecoderzhi@gmail.com>
2021-09-08 15:41:36 -0700

    asset upper bound

---
+++ contracts/AssetUpperBound.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AssetUpperBound is Ownable {
+    event UppserBoundSet(address indexed asset, uint256 upperBound);
+
+    mapping(address => uint256) public upperBounds;
+
+    function setUpperBound(address _asset, uint256 _upperBound) public onlyOwner {
+        upperBounds[_asset] = _upperBound;
+        emit UppserBoundSet(_asset, _upperBound);
+    }
+}
