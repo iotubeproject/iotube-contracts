855ef17e4bd8129108b0056211b342c21b4242a1
zhi <thecoderzhi@gmail.com>
2022-04-12 18:45:52 -0700

    clean up testimony dao

--- contracts/TestimonyDAO.sol
+++ contracts/TestimonyDAO.sol
@@ -5,15 +5,7 @@ pragma solidity >=0.8.0;
 contract TestimonyDAO {
     event Testimony(address indexed validator, bytes32 indexed key, bytes testimony);
 
-    // mapping(address => mapping(bytes32 => bytes)) public testimonies;
-
     function addTestimony(bytes32 key, bytes calldata value) public {
-        // require(testimonies[msg.sender][key].length == 0, "testimony exists");
-        // testimonies[msg.sender][key] = value;
         emit Testimony(msg.sender, key, value);
     }
-
-    // function getTestimony(address validator, bytes32 key) public view returns (bytes memory) {
-    //     return testimonies[msg.sender][key];
-    // }
 }
