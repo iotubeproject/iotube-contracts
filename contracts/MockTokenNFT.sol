c6f7d74dfe0d76c4d6fdc9be7df12ef797ba0030
huof6890 <68298506@qq.com>
2022-04-15 18:40:51 +0800

    merge

--- contracts/MockTokenNFT.sol
+++ contracts/MockTokenNFT.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-<<<<<<< HEAD
-pragma solidity 0.7.6;
-
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
-=======
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
->>>>>>> lord_test_v2
 
 contract MockTokenNFT is ERC721Burnable, Ownable {
     constructor(string memory name_, string memory symbol_) public ERC721(name_, symbol_) {}
@@ -22,8 +15,4 @@ contract MockTokenNFT is ERC721Burnable, Ownable {
     ) external onlyOwner {
         _safeMint(recipient, tokenID, data);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> lord_test_v2
