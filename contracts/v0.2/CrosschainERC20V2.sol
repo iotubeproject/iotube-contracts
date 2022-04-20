a034609dbdebac4e2392870478b90f72f52f228c
CoderZhi <thecoderzhi@gmail.com>
2022-04-19 22:02:43 -0700

    Update CrosschainERC20V2.sol

--- contracts/v0.2/CrosschainERC20V2.sol
+++ contracts/v0.2/CrosschainERC20V2.sol
@@ -5,30 +5,23 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-interface ITubeRegistry {
-    function canMintCrosschainERC20(address token, address account) external view returns (bool);
-
-    function registerCrosschainERC20Wrapper(address _wrapper) external;
+interface IMinterDAO {
+    function isMinter(address account, address token) external view returns (bool);
 }
 
 contract CrosschainERC20V2 is ERC20Burnable {
     using SafeERC20 for IERC20;
 
-    modifier onlyMinter() {
-        require(tubeRegistry.canMintCrosschainERC20(address(this), msg.sender), "not the minter");
-        _;
-    }
-
     uint8 private decimals_;
-    ITubeRegistry public tubeRegistry;
+    IMinterDAO public minterDAO;
 
     constructor(
-        address _tubeRegistry,
+        address _minterDAO,
         string memory _name,
         string memory _symbol,
         uint8 _decimals
     ) ERC20(_name, _symbol) {
-        tubeRegistry = ITubeRegistry(_tubeRegistry);
+        minterDAO = IMinterDAO(_minterDAO);
         decimals_  = _decimals;
     }
 
@@ -36,7 +29,8 @@ contract CrosschainERC20V2 is ERC20Burnable {
         return decimals_;
     }
 
-    function mint(address _to, uint256 _amount) public onlyMinter returns (bool) {
+    function mint(address _to, uint256 _amount) public returns (bool) {
+        require(minterDAO.isMinter(msg.sender, address(this)), "not the minter");
         require(_amount != 0, "amount is 0");
         _mint(_to, _amount);
         return true;
